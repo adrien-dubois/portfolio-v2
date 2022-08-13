572ffd99ea2ec2b20cac361a6309ba5996589821
adrien-dubois <mcsnoos@gmail.com>
2022-08-13 23:19:24 +0200

    [WIP]Setup Firebase

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,13 @@
+
+
+const Login = () => {
+  return (
+    <div className="dashboard">
+        <button >
+            Connexion avec Google
+        </button>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = 
+{
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PORJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
