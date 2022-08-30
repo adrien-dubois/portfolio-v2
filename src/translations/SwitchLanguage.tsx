81cf44cc18dd0025d2b03ffeaa0103f3263914dc
adrien-dubois <mcsnoos@gmail.com>
2022-08-30 21:09:11 +0200

    [EDIT]Change translation css

--- src/translations/SwitchLanguage.tsx
+++ src/translations/SwitchLanguage.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
-import { FrFlag, UkFlag } from '../assets/flags';
-import Flag from './Flag';
-import Switch from './Switch';
+import { Wrapper } from './Switch.elements';
 
 
 const SwitchLanguage = () => {
@@ -22,63 +19,13 @@ const SwitchLanguage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isToggle]);
 
-    const selectedLanguage = i18n.language;
-
 
   return (
-
-    <FlagContainer>
-        <Flag
-            image={FrFlag}
-            isSelected={ selectedLanguage === 'fr' }
-        />
-        <Switch isToggled={isToggle} onToggle={() => setIsToggle(!isToggle)} />
-        <Flag
-            image={UkFlag}
-            isSelected={ selectedLanguage === 'en' }
-        />
-    </FlagContainer>
+    <Wrapper>
+        <input type="checkbox" name="checkbox" className='switch' checked={isToggle} onChange={() => setIsToggle(!isToggle)} />
+    </Wrapper>
 
   )
 }
 
-const FlagContainer = styled.div`
-    position: absolute;
-    top: -1%;
-    right: 0;
-    width: 230px;
-    display: flex;
-    justify-content: space-evenly;
-    z-index: 5;
-    opacity: 0;
-    animation: fadeIn 1s 1.5s;
-    animation-fill-mode: forwards;
-
-    @media screen and (max-width: 1200px) {
-        width: 50%;
-        margin: 0 auto;
-        left: 0;
-        top: 0; 
-        margin-bottom: 10px;
-        z-index: 1;
-    }
-
-    label{
-        margin: 0 5px;
-        margin-top: 16px;
-
-        @media screen and (max-width: 1200px) {
-            margin-top: 10px;
-        }
-    }
-
-    img{
-        width: 40px;
-
-        @media screen and (max-width: 1200px) {
-            width: 30px;
-        }
-    }
-`
-
-export default SwitchLanguage
+export default SwitchLanguage;
